158a9a1663d04ae0eb2e660e6757eb8f692cc03a
AlexJohannson <mehedsanya@gmail.com>
2024-12-08 13:43:07 +0100

    [typescript]/typescript

--- Demo2/main.js
+++ Demo2/main.js
@@ -1,7 +1,48 @@
+var x = 10;
+console.log(x);
 function foo(o) {
     return o + '';
 }
-foo(12000);
+console.log(foo(12000));
 // foo('12000'); error
 // foo([]); error
 // foo({}); error
+function first(o, x) {
+    return o + '';
+}
+console.log(first(35, 'okten'));
+function bar(object) {
+}
+console.log(bar({ id: 1, name: 'okten' }));
+var n = 2;
+console.log(n);
+var s = 'oktetn';
+console.log(s);
+var b = true;
+console.log(b);
+var obj = { id: 1, status: false };
+console.log(obj);
+var arrS = ['okten', 'school', 'typescript'];
+console.log(arrS);
+var arrN = [1, 2, 3, 4, 5];
+console.log(arrN);
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+var arrObj = [{ name: 'okten', age: 1 }, { name: 'okten', age: 1 }, { name: 'okten', age: 1 }];
+console.log(arrObj);
+function asd() {
+    return { name: 'okten' };
+}
+console.log(asd());
+function divCreator(content, domElementToAdd) {
+    var div = document.createElement('div');
+}
+divCreator('okten', document.body);
+function fetchData() {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(function (value) { return value.json(); })
+        .then(function (value) {
+        return value;
+    });
+}
+console.log(fetchData());
--- Demo2/main.ts
+++ Demo2/main.ts
@@ -1,8 +1,66 @@
-function foo(o: number){
+let x = 10;
+console.log(x)
+
+function foo(o: number): string{
      return o + '';
 }
-
-foo(12000);
+console.log(foo(12000));
 // foo('12000'); error
 // foo([]); error
 // foo({}); error
+
+function first(o: number, x: string): string {
+     return o + '';
+}
+console.log(first(35, 'okten'));
+
+function bar(object:{id: number, name: string}): void{
+
+}
+console.log(bar({id: 1, name: 'okten'}));
+
+let n: number = 2;
+console.log(n);
+
+let s: string = 'oktetn';
+console.log(s);
+
+let b:boolean = true;
+console.log(b);
+
+let obj: {id: number, status: boolean} = {id: 1, status: false};
+console.log(obj);
+
+let arrS: string[] = ['okten', 'school', 'typescript'];
+console.log(arrS);
+
+let arrN: number[] = [1, 2, 3, 4, 5];
+console.log(arrN);
+
+let arr: Array<number> = [1, 2, 3, 4, 5];
+console.log(arr);
+
+let arrObj: {name: string, age: number} [] = [{name: 'okten', age:1}, {name: 'okten', age:1}, {name: 'okten', age:1}];
+console.log(arrObj);
+
+function asd(): { name: string } {
+     return {name: 'okten'};
+}
+console.log(asd());
+
+function divCreator(content: string, domElementToAdd: HTMLElement) {
+     let div:HTMLDivElement = document.createElement('div');
+
+}
+
+divCreator('okten', document.body);
+
+function fetchData(): Promise <{id: number, usrId:number, title: string, body: string}[]>{
+     return fetch('https://jsonplaceholder.typicode.com/posts')
+         .then(value => value.json())
+         .then((value: {id: number, usrId:number, title: string, body: string}[]) => {
+              return value;
+         })
+}
+
+console.log(fetchData());
