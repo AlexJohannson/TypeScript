60f074f238e246166c263dc3d956a7c35d03e905
AlexJohannson <mehedsanya@gmail.com>
2024-12-09 11:34:03 +0100

    [update]:TypeScript/Task_1/Task_1-3/index.html, Main.ts, Main.js

---
+++ Task_1/Task_1-3/Main.js
@@ -0,0 +1,6 @@
+var number = 100;
+console.log(typeof number);
+var string = '100';
+console.log(typeof string);
+var boolean = true;
+console.log(typeof boolean);
---
+++ Task_1/Task_1-3/Main.ts
@@ -0,0 +1,8 @@
+let number: number = 100;
+console.log(typeof number);
+
+let string: string = '100';
+console.log(typeof  string);
+
+let boolean: boolean = true;
+console.log(typeof  boolean);
