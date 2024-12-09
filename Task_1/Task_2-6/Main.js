71b8f4c4cd7fec8f4b0d81b321ffccd9063037f3
AlexJohannson <mehedsanya@gmail.com>
2024-12-09 13:11:33 +0100

    [update]:TypeScript/Task_1/Task_2-6/index.html, Main.ts, Main.js

---
+++ Task_1/Task_2-6/Main.js
@@ -0,0 +1,21 @@
+var w = 5;
+if (w === 0) {
+    console.log('True');
+}
+else {
+    console.log('False');
+}
+var q = 1;
+if (w === q) {
+    console.log('True');
+}
+else {
+    console.log('False');
+}
+var t = -3;
+if (w === t) {
+    console.log('True');
+}
+else {
+    console.log('False');
+}
---
+++ Task_1/Task_2-6/Main.ts
@@ -0,0 +1,23 @@
+let w: number = 5;
+
+if(w === 0) {
+    console.log('True');
+} else {
+    console.log('False');
+}
+
+let q: number = 1;
+
+if(w === q) {
+    console.log('True');
+} else {
+    console.log('False');
+}
+
+let t: number = -3;
+
+if(w === t) {
+    console.log('True');
+} else {
+    console.log('False');
+}
