90cd95539bd2ee5e331e71e3f2219c3c707c5fba
AlexJohannson <mehedsanya@gmail.com>
2024-12-09 11:53:09 +0100

    [update]:TypeScript/Task_1/Task_2-2/index.html, Main.ts, Main.js

---
+++ Task_1/Task_2-2/Main.js
@@ -0,0 +1,18 @@
+var book1 = {
+    title: 'Title',
+    pageCount: 405,
+    genre: 'Thriller'
+};
+console.log(book1);
+var book2 = {
+    title: 'Title',
+    pageCount: 276,
+    genre: 'Novel'
+};
+console.log(book2);
+var book3 = {
+    title: 'Title',
+    pageCount: 115,
+    genre: 'Fantasy'
+};
+console.log(book3);
---
+++ Task_1/Task_2-2/Main.ts
@@ -0,0 +1,28 @@
+type Book = {
+    title: string;
+    pageCount: number;
+    genre: string
+};
+
+
+
+let book1: Book = {
+    title: 'Title',
+    pageCount: 405,
+    genre: 'Thriller'
+}
+console.log(book1);
+
+let book2: Book = {
+    title: 'Title',
+    pageCount: 276,
+    genre: 'Novel'
+}
+console.log(book2);
+
+let book3: Book = {
+    title: 'Title',
+    pageCount: 115,
+    genre: 'Fantasy'
+}
+console.log(book3);
