34d63469e23f7e56b085e90852689e7b812cf971
AlexJohannson <mehedsanya@gmail.com>
2024-12-09 14:36:11 +0100

    [update]:TypeScript/Task_1/Task_3-4/index.html, Main.ts, Main.js

---
+++ Task_1/Task_3-4/Main.js
@@ -0,0 +1,5 @@
+var z = 0;
+while (z < 20) {
+    document.write("<h1>Oleksandr ".concat(z, "</h1>"));
+    z++;
+}
---
+++ Task_1/Task_3-4/Main.ts
@@ -0,0 +1,6 @@
+let z: number = 0;
+
+while (z < 20) {
+    document.write(`<h1>Oleksandr ${z}</h1>`);
+    z++;
+}
