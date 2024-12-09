0de826d23be55fd847dd1bbc2dedd408270b4d9a
AlexJohannson <mehedsanya@gmail.com>
2024-12-09 11:27:43 +0100

    [update]:TypeScript/Task_1/Task_1-2/index.html, Main.ts, Main.js

---
+++ Task_1/Task_1-2/Main.js
@@ -0,0 +1,5 @@
+var firstName = 'Oleksandr ';
+var middleName = 'Mykolayovych ';
+var lastName = 'Mekhed';
+var person = "".concat(firstName, " ").concat(middleName, "  ").concat(lastName);
+console.log(person);
---
+++ Task_1/Task_1-2/Main.ts
@@ -0,0 +1,7 @@
+let firstName: string = 'Oleksandr ';
+let middleName: string = 'Mykolayovych ';
+let lastName: string = 'Mekhed';
+
+let person: string = `${firstName} ${middleName}  ${lastName}`;
+
+console.log(person);
