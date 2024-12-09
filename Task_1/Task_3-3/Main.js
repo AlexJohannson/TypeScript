5b9a7304165658114c22f4b997cca2709c851bb0
AlexJohannson <mehedsanya@gmail.com>
2024-12-09 14:32:17 +0100

    [update]:TypeScript/Task_1/Task_3-3/index.html, Main.ts, Main.js

---
+++ Task_1/Task_3-3/Main.js
@@ -0,0 +1,5 @@
+var v = 0;
+while (v < 20) {
+    document.write('<h1>' + 'Oleksandr' + '</h1>');
+    v++;
+}
---
+++ Task_1/Task_3-3/Main.ts
@@ -0,0 +1,6 @@
+let v: number = 0;
+
+while (v < 20) {
+    document.write('<h1>' + 'Oleksandr' + '</h1>');
+    v++;
+}
