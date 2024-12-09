32d6740ede4d7d293aa322090bfe2722ffd91a3c
AlexJohannson <mehedsanya@gmail.com>
2024-12-09 11:44:08 +0100

    [update]:TypeScript/Task_1/Task_2-1/index.html, Main.ts, Main.js

---
+++ Task_1/Task_2-1/Main.js
@@ -0,0 +1,6 @@
+var arrayNumder = [123, -4, 8739, 3467];
+console.log(arrayNumder);
+var arrayString = ['name', 'password', 'email', 'java script'];
+console.log(arrayString);
+var arrayBoolean = [false, true];
+console.log(arrayBoolean);
---
+++ Task_1/Task_2-1/Main.ts
@@ -0,0 +1,8 @@
+let arrayNumder: number[] = [123, -4, 8739, 3467];
+console.log(arrayNumder);
+
+let arrayString: string[] = ['name', 'password', 'email', 'java script'];
+console.log(arrayString);
+
+let arrayBoolean: boolean[] = [false, true];
+console.log(arrayBoolean);
