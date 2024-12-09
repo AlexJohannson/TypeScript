cac05a631fbce91f60894305ec11a5f6c9759c76
AlexJohannson <mehedsanya@gmail.com>
2024-12-09 12:38:49 +0100

    [update]:TypeScript/Task_1/Task_2-3/index.html, Main.ts, Main.js

---
+++ Task_1/Task_2-3/Main.js
@@ -0,0 +1,72 @@
+var objectUsers = [
+    {
+        id: 1,
+        name: 'Oleksandr',
+        username: 'Oleksandr_Oleksandr',
+        password: '@oleksandr'
+    },
+    {
+        id: 2,
+        name: 'Vasia',
+        username: 'Vasia_Vasia',
+        password: '@vasia'
+    },
+    {
+        id: 3,
+        name: 'Virginia',
+        username: 'Virginia_Virginia',
+        password: '@virginia'
+    },
+    {
+        id: 4,
+        name: 'Mykola',
+        username: 'Mykola_mykola',
+        password: '@mykola'
+    },
+    {
+        id: 5,
+        name: 'Vadym',
+        username: 'Vadym_Vadym',
+        password: '@vadym'
+    },
+    {
+        id: 6,
+        name: 'Dmytro',
+        username: 'Dmytro_Dmytro',
+        password: '@dmytro'
+    },
+    {
+        id: 7,
+        name: 'Inna',
+        username: 'Inna_Inna',
+        password: '@inna'
+    },
+    {
+        id: 8,
+        name: 'Svitlana',
+        username: 'Svitlana_Svitlana',
+        password: '@svitlana'
+    },
+    {
+        id: 9,
+        name: 'Olha',
+        username: 'Olha_Olha',
+        password: '@olha'
+    },
+    {
+        id: 10,
+        name: 'Roman',
+        username: 'Roman_Roman',
+        password: '@roman'
+    }
+];
+console.log(objectUsers[0].password);
+console.log(objectUsers[1].password);
+console.log(objectUsers[2].password);
+console.log(objectUsers[3].password);
+console.log(objectUsers[4].password);
+console.log(objectUsers[5].password);
+console.log(objectUsers[6].password);
+console.log(objectUsers[7].password);
+console.log(objectUsers[8].password);
+console.log(objectUsers[9].password);
