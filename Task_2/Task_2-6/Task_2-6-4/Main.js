599a14484035bdcf7bad6f0832a2b0045ab746a8
AlexJohannson <mehedsanya@gmail.com>
2024-12-10 15:31:04 +0100

    [update]:TypeScript/Task_2/Task_2-6/Task_2-6-4/index.html, Main.ts, Main.js

---
+++ Task_2/Task_2-6/Task_2-6-4/Main.js
@@ -0,0 +1,5 @@
+var str = ' dirty string   ';
+var trim = str.trim();
+console.log(trim);
+console.log(trim.length);
+console.log(str.length);
---
+++ Task_2/Task_2-6/Task_2-6-4/Main.ts
@@ -0,0 +1,6 @@
+let str: string = ' dirty string   ';
+
+const trim: string = str.trim();
+console.log(trim);
+console.log(trim.length);
+console.log(str.length);
