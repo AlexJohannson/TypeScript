d4474335dcb5ced636f3a940166294be8a68bc8a
AlexJohannson <mehedsanya@gmail.com>
2024-12-10 14:37:57 +0100

    [update]:TypeScript/Task_2/Task_2-5/Task_2-5-10/index.html, Main.ts, Main.js

---
+++ Task_2/Task_2-5/Task_2-5-10/Main.ts
@@ -0,0 +1,14 @@
+let arr: number[] = [12, 45, 990, 2, 54, 8377, 23, -12, 45, -76];
+
+let minimumNumber = (numbers: number[]) => {
+    let min: number = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        if (number < min) {
+            min = number;
+        }
+    }
+    return min;
+}
+minimumNumber(arr);
+console.log(minimumNumber(arr));
