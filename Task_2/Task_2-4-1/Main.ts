1ce804f6cd906bbf34c9052466f9b7980a3c3234
AlexJohannson <mehedsanya@gmail.com>
2024-12-10 19:21:07 +0100

    [add]:TypeScript/Task_2/Task_2-6/Task_2-6-11/index.html, Main.ts, Main.js

--- Task_2/Task_2-4-1/Main.ts
+++ Task_2/Task_2-4-1/Main.ts
@@ -1,4 +1,4 @@
-function square(aNun: number, bNun: number): number {
+function square1(aNun: number, bNun: number): number {
     const result: number = aNun * bNun;
     console.log(result);
     return result;
