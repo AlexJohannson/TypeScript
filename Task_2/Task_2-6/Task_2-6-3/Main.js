99e37d57efd30a59b1b6d0b7680ba2d0fe16b902
AlexJohannson <mehedsanya@gmail.com>
2024-12-10 15:27:47 +0100

    [update]:TypeScript/Task_2/Task_2-6/Task_2-6-3/index.html, Main.ts, Main.js

---
+++ Task_2/Task_2-6/Task_2-6-3/Main.js
@@ -0,0 +1,9 @@
+var a = 'HELLO WORLD';
+var aLowerCase = a.toLowerCase();
+console.log(aLowerCase);
+var b = 'LOREM IPSUM';
+var bLowerCase = b.toLowerCase();
+console.log(bLowerCase);
+var c = 'JAVASCRIPT IS COOL';
+var cLowerCase = c.toLowerCase();
+console.log(cLowerCase);
---
+++ Task_2/Task_2-6/Task_2-6-3/Main.ts
@@ -0,0 +1,11 @@
+let a: string = 'HELLO WORLD';
+const aLowerCase: string = a.toLowerCase();
+console.log(aLowerCase);
+
+let b: string = 'LOREM IPSUM';
+const bLowerCase: string = b.toLowerCase();
+console.log(bLowerCase);
+
+let c: string = 'JAVASCRIPT IS COOL';
+const cLowerCase: string = c.toLowerCase();
+console.log(cLowerCase);
