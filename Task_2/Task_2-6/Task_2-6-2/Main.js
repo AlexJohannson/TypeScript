220a145f76dbf7e7e5dd127fa63a001223820b04
AlexJohannson <mehedsanya@gmail.com>
2024-12-10 15:24:24 +0100

    [update]:TypeScript/Task_2/Task_2-6/Task_2-6-2/index.html, Main.ts, Main.js

---
+++ Task_2/Task_2-6/Task_2-6-2/Main.js
@@ -0,0 +1,9 @@
+var a = 'hello world';
+var aUpperCase = a.toUpperCase();
+console.log(aUpperCase);
+var b = 'lorem ipsum';
+var bUpperCase = b.toUpperCase();
+console.log(bUpperCase);
+var c = 'javascript is cool';
+var cUpperCase = c.toUpperCase();
+console.log(cUpperCase);
---
+++ Task_2/Task_2-6/Task_2-6-2/Main.ts
@@ -0,0 +1,11 @@
+let a: string = 'hello world';
+const aUpperCase: string = a.toUpperCase();
+console.log(aUpperCase);
+
+let b: string = 'lorem ipsum';
+const bUpperCase: string = b.toUpperCase();
+console.log(bUpperCase);
+
+let c: string = 'javascript is cool';
+const cUpperCase: string = c.toUpperCase()
+console.log(cUpperCase);
